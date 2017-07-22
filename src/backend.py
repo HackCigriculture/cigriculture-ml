e50a9dca732d4d24fc8bdac534d50afdcb374bd8
Delton Ding <dsh0416@gmail.com>
2017-07-22 11:12:09 +0800

    Add python backend

---
+++ src/backend.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, request
+import numpy as np
+from network import network
+
+app = Flask(__name__)
+model = network()
+model.load_weights('model/1500690378.result')
+
+
+@app.route('/', methods=['POST'])
+def geo():
+    body = request.get_json(silent=True, force=True)
+    result = model.predict(np.array(body))
+    return json.dumps(result.tolist())
+
+if __name__ == '__main__':
+    app.run()
